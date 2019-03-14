c8392735df09e6986bbfae24f15ec911f6c291cd
solaire-off <victor374288@mail.ru>
2019-03-14 21:08:37 +0300

    Save/restore dash settings (with redux)

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,21 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import { rootReducer } from '../reducers'
+import { saveState, loadState } from '../localStorage';
+import throttle from 'lodash/throttle';
 // For fetch in actions (avalible return function from action)
 import thunk from 'redux-thunk'
 
-export const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
+
+const initState = loadState()
+
+export const store = createStore(
+  rootReducer,
+  initState,
+  compose(applyMiddleware(thunk)),
+);
+
+store.subscribe(throttle(() => {
+  saveState({
+    dash : store.getState().dash 
+  })
+}), 1000)
